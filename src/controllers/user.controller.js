fe0df928c996bceaf2f238e3b33b93c8f8850d61
Chirag Bansal <chiragb3005@gmail.com>
2026-04-29 23:40:52 +0530

    hehe

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 
 const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({
-        message: "ok"
+        message: "chal gya bina error ke !! YAY ...."
     })
 })
 

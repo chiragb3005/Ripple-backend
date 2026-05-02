76d3d834918f86d645d70dd858ae6ccd29925bcf
Chirag Bansal <chiragb3005@gmail.com>
2026-05-02 15:13:05 +0530

    fixed server run error

--- src/utils/deleteFromCloudinary.js
+++ src/utils/deleteFromCloudinary.js
@@ -1,4 +1,4 @@
-import { ApiError } from "./ApiError"
+import { ApiError } from "./ApiError.js"
 import { v2 as cloudinary } from 'cloudinary'
 
 export const deleteFromCloudinary = async (imageUrl) => {
